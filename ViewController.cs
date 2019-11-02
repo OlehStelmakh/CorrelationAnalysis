3fb58fbe6236b3f26181fd479cec9429558433a3
Oleh Stelmakh <olehstelmakh@gmail.com>
2019-11-02 02:08:10 +0200

    Create first page of app

--- ViewController.cs
+++ ViewController.cs
@@ -6,6 +6,8 @@ namespace ChoiceApp
 {
     public partial class ViewController : UIViewController
     {
+        private bool accessForNext = false;
+
         public ViewController(IntPtr handle) : base(handle)
         {
         }
@@ -13,11 +15,6 @@ namespace ChoiceApp
         public override void ViewDidLoad()
         {
             base.ViewDidLoad();
-            SwitchXY.On = false;
-            SwitchCov.On = false;
-            SwitchStandDev.On = false;
-            SwitchAmount.On = false;
-            SwitchZScore.On = false;
             // Perform any additional setup after loading the view, typically from a nib.
         }
 
@@ -42,19 +39,138 @@ namespace ChoiceApp
 
         partial void UIButton205_TouchUpInside(UIButton sender)
         {
-            
+          
         }
 
+        //X and Y fields
         partial void UISwitch202_TouchUpInside(UISwitch sender)
         {
             if(SwitchXY.On)
             {
                 SwitchCov.On = false;
+                SwitchAmount.On = false;
+                SwitchMean.On = false;
+                SwitchStandDev.On = false;
+                SwitchZScore.On = false;
+                sufficient();
+            }
+            else { notSufficient(); }
+
+
+        }
+
+        //Cov
+        partial void UISwitch203_TouchUpInside(UISwitch sender)
+        {
+            if (SwitchCov.On)
+            {
+                SwitchXY.On = false;
+                SwitchZScore.On = false;
+                SwitchAmount.On = false;
+                SwitchMean.On = true;
+                SwitchStandDev.On = true;
+                sufficient();
+            }
+            else { notSufficient(); }
+
+        }
+
+        //Z-score
+        partial void UISwitch2659_TouchUpInside(UISwitch sender)
+        {
+            if (SwitchZScore.On)
+            {
+                SwitchXY.On = false;
+                SwitchCov.On = false;
+                SwitchAmount.On = true;
+                SwitchStandDev.On = true;
+                SwitchMean.On = true;
+                sufficient();
+
             }
             else
             {
-                SwitchCov.On = true;
+                SwitchAmount.On = false;
+                notSufficient();
+            }
+          
+        }
+
+        //Amount
+        partial void UISwitch2658_TouchUpInside(UISwitch sender)
+        {
+            if (SwitchAmount.On)
+            {
+                SwitchXY.On = false;
+                SwitchCov.On = false;
+                SwitchZScore.On = true;
+                SwitchStandDev.On = true;
+                SwitchMean.On = true;
+                accessForNext = true;
+                sufficient();
+            }
+            else
+            {
+                SwitchZScore.On = false;
+                notSufficient();
+            }
+           
+            
+
+        }
+
+        private void notSufficient()
+        {
+            LabelSufficient.TextColor = UIColor.SystemRedColor;
+            LabelSufficient.Text = "This information is not sufficient for calculations";
+            LabelSufficient.Hidden = false;
+            ButtonFirstView.Enabled = false;
+        }
+
+        private void sufficient()
+        {
+            LabelSufficient.Text = "This information is sufficient for calculations";
+            LabelSufficient.TextColor = UIColor.SystemGreenColor;
+            LabelSufficient.Hidden = false;
+            ButtonFirstView.Enabled = true;
+        }
+
+        /*
+        //Mean of X and Y
+        partial void UISwitch16249_TouchUpInside(UISwitch sender)
+        {
+            if (SwitchMean.On)
+            {
+                SwitchXY.On = false;
+                LabelSufficient.Hidden = true;
+            }
+            else
+            {
+                SwitchCov.On = false;
+                SwitchStandDev.On = false;
+                SwitchZScore.On = false;
+                SwitchAmount.On = false;
+                LabelSufficient.Hidden = true;
             }
         }
+
+        //Standart dev
+        partial void UISwitch2657_TouchUpInside(UISwitch sender)
+        {
+            if (SwitchStandDev.On)
+            {
+                SwitchXY.On = false;
+                LabelSufficient.Hidden = true;
+            }
+            else
+            {
+                SwitchCov.On = false;
+                SwitchMean.On = false;
+                SwitchZScore.On = false;
+                SwitchAmount.On = false;
+                LabelSufficient.Hidden = true;
+            }
+        }
+        */
     }
 }
--- ViewController.designer.cs
+++ ViewController.designer.cs
@@ -16,11 +16,23 @@ namespace ChoiceApp
     {
         [Outlet]
         [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton ButtonFirstView { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel LabelSufficient { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
         UIKit.UISwitch SwitchAmount { get; set; }
 
         [Outlet]
         [GeneratedCode ("iOS Designer", "1.0")]
         UIKit.UISwitch SwitchCov { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UISwitch SwitchMean { get; set; }
 
         [Outlet]
         [GeneratedCode ("iOS Designer", "1.0")]
@@ -38,12 +50,42 @@ namespace ChoiceApp
         [GeneratedCode ("iOS Designer", "1.0")]
         partial void UIButton205_TouchUpInside (UIKit.UIButton sender);
 
+        [Action ("UISwitch16249_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void UISwitch16249_TouchUpInside (UIKit.UISwitch sender);
+
         [Action ("UISwitch202_TouchUpInside:")]
         [GeneratedCode ("iOS Designer", "1.0")]
         partial void UISwitch202_TouchUpInside (UIKit.UISwitch sender);
 
+        [Action ("UISwitch203_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void UISwitch203_TouchUpInside (UIKit.UISwitch sender);
+
+        [Action ("UISwitch2657_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void UISwitch2657_TouchUpInside (UIKit.UISwitch sender);
+
+        [Action ("UISwitch2658_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void UISwitch2658_TouchUpInside (UIKit.UISwitch sender);
+
+        [Action ("UISwitch2659_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void UISwitch2659_TouchUpInside (UIKit.UISwitch sender);
+
         void ReleaseDesignerOutlets ()
         {
+            if (ButtonFirstView != null) {
+                ButtonFirstView.Dispose ();
+                ButtonFirstView = null;
+            }
+
+            if (LabelSufficient != null) {
+                LabelSufficient.Dispose ();
+                LabelSufficient = null;
+            }
+
             if (SwitchAmount != null) {
                 SwitchAmount.Dispose ();
                 SwitchAmount = null;
@@ -52,6 +94,11 @@ namespace ChoiceApp
             if (SwitchCov != null) {
                 SwitchCov.Dispose ();
                 SwitchCov = null;
+            }
+
+            if (SwitchMean != null) {
+                SwitchMean.Dispose ();
+                SwitchMean = null;
             }
 
             if (SwitchStandDev != null) {
